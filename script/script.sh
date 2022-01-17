fa1cb32223369e6174f360eafc8c7938513c1628
Eugenio Duarte <eduarte@Eugenios-MacBook-Pro.local>
2022-01-16 20:35:21 -0500

    init commit

---
+++ script/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#sudo yum update -y
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+sudo systemctl start docker
+sudo yum install wget -y
+sudo sudo yum install git -y
