8d0fee51099b7fd708e95b797a0e74aedaf5f40a
Eugenio Duarte <eduarte@Eugenios-MacBook-Pro.local>
2022-03-08 14:02:42 -0500

    updating ssh keys

--- ssh_keys.tf
+++ ssh_keys.tf
@@ -1,7 +1,7 @@
 # Public Key for accessing EC2 instances
 
-resource "aws_key_pair" "cloudacia-key" {
-  key_name   = "user-key"
+resource "aws_key_pair" "user_key" {
+  key_name   = "cloudacia-key"
   public_key = var.ssh_public_key
 
   tags = {
